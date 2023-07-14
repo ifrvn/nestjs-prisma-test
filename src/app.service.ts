ba42a5546cc6f1db9ea49828e17b95f44fdb2a19
Frank <frank@ifrank.cn>
2023-07-14 09:52:40 +0800

    test code

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly prisma: PrismaService) {}
+
+  async user(where: Prisma.UserWhereUniqueInput, include?: Prisma.UserInclude) {
+    const user = await this.prisma.user.findUnique({
+      where,
+      include,
+    });
+    return user;
+  }
+
+  async test() {
+    const user = await this.user({ id: 1 }, { profile: true });
+    return user?.profile; // here's the problem
   }
 }
